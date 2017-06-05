46795cb6c5ab72fb44f77340f599bfde1769af9a
colttylerwilson <colt.wilson@sjsu.edu>
2017-06-05 16:04:39 -0700

    first commit

---
+++ website.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+var path = __dirname + '/views/';
+
+//You need to tell express from where should it serve static content.
+app.use(express.static('public'));
+
+app.use('/', router);
+
+router.get('/', function (req, res) {
+    res.sendFile(path + 'index.html');
+});
+
+router.get('/projects', function (req, res) {
+    res.sendFile(path + 'projects.html');
+});
+
+router.get('/contact', function (req, res) {
+    res.sendFile(path + 'contact.html');
+});
+
+app.use('*', function (req, res) {
+    res.send('Error 404: Not Found!');
+});
+
+app.listen(3000, function () {
+    console.log("Server running at Port 3000");
+});
