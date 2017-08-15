2b6748b5f81661a4bddf3abdee916fc8abab116f
colttylerwilson <colt.wilson@sjsu.edu>
2017-08-15 16:04:51 -0700

    Changed some things for Openshift deployment.

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var router = express.Router();
 
 var path = __dirname + '/views/';
 
+
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
 //You need to tell express from where should it serve static content.
 app.use(express.static('public'));
 
